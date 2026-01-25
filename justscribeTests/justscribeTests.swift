a83e87bea9e2074cbbebec0050ee6e1a78528557
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2026-01-25 17:46:52 +0100

    Initial commit

---
+++ justscribeTests/justscribeTests.swift
@@ -0,0 +1,16 @@
+//
+//  justscribeTests.swift
+//  justscribeTests
+//
+//  Created by Antoni Silvestrovic on 24/01/2026.
+//
+
+import Testing
+
+struct justscribeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
